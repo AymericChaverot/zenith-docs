090e90db2a3983034a7bc3d02587bc98a7d668e3
Aymeric CHAVEROT <aymeric.chaverot@protonmail.com>
2026-09-16 23:49:22 +0200

    remove the fake v1 from the docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -36,10 +36,6 @@ export default defineConfig({
         root: { label: 'English' },
         fr: { label: 'Français' },
       },
-      versions: {
-        root: { label: 'v2', badge: 'Latest' },
-        v1: { label: 'v1' },
-      },
     }),
   ],
 });
